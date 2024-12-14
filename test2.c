7f9fef8621ac28471e861f4da6fb6cbb6138da3d
SAMIR NEZAMETDINOV <nezametdinovsamir@gmail.com>
2024-12-14 09:10:33 +0300

    подправил dump

--- test2.c
+++ test2.c
@@ -14,9 +14,10 @@ struct stack
 
 const stack_elem CANARY = (stack_elem)0xBADC0FFEE;
 
-const stack_elem POISON = (stack_elem)0xBAD1ABA
+const stack_elem POISON = (stack_elem)0xBAD1ABA;
 
-int put_canary(stack *stk)
+
+int put_canary(struct stack *stk)
 {
     stk->data[0] = CANARY;
     stk->data[stk->capacity + 1] = CANARY;
@@ -25,19 +26,19 @@ int put_canary(stack *stk)
 }
 
 int stack_destructor(struct stack* stk) {  // исправить, попортить данные
-    free(stk->data);
+    free(stk->data);                       // сделать проверки
     stk->data = NULL;
 
     return 0;
 }
 
-int stack_constructor(struct stack * stk, int capacity) {
+int stack_constructor(struct stack * stk, int capacity) {  // сделать нормальную проверку
     stk->data = (stack_elem *)calloc(capacity + 2, sizeof(stack_elem));
 
     if (stk->data == NULL)
     {
         printf("memory allocation error\n");
-        return ; // добавить название
+        return -1; // добавить название
     }
 
     stk->size = 0;
@@ -48,21 +49,22 @@ int stack_constructor(struct stack * stk, int capacity) {
 }
 
 int stack_push(struct stack*stk, stack_elem value) {
-    assert(stk->size < stk->capacity);  // убрать поставить ретурн
+    assert(stk->size < stk->capacity);  // сделать проверки
     stk->data[stk->size + 1] = value;
     (stk->size)++;
     return 0;
 }
 
-int stack_pop(struct stack*stk) { // возратить элемент
-    assert(stk->size > 0);  // убрать поставить ретурн
+int stack_pop(struct stack*stk, stack_elem *pop_elem) { // возратить элемент
+    assert(stk->size > 0);  // нормальная проверка
     stk->size--;
-    *elem_from_stack = stk->data[stk->size + 1];
+    *pop_elem = stk->data[stk->size + 1];
     stk->data[stk->size + 1] = POISON;
+    return 0;
 }
 
 int stack_dump(struct stack*stk) {
-    for (int i = 0; i < stk->size; ++i) {
+    for (int i = 1; i < (stk->size) + 1; ++i) {
         printf("%lg ", stk->data[i]);
     }
     printf("\n"
@@ -83,17 +85,20 @@ int verificator(struct stack * stk) {  // добавить канарейку
 }
 
 int main() {
-    struct stack stk;
+    struct stack stk = {NULL, 0, 0};
+    stack_elem pop_elem = 0;
     stack_constructor(&stk, 6);
-    stack_push(&stk, 3);
-    stack_push(&stk, 4);
-    stack_push(&stk, 4);
-    stack_pop(&stk);
-    stack_pop(&stk);
-    stack_pop(&stk);
-    stack_pop(&stk);
-    verificator(&stk);
+    stack_push(&stk, 6);
+    stack_push(&stk, 6);
+    stack_push(&stk, 6);
+    stack_push(&stk, 6);
+    stack_push(&stk, 6);
+    stack_push(&stk, 6);
+    stack_pop(&stk, &pop_elem);
+    // printf("%lg", stk.data[1]);
+    // verificator(&stk);
     stack_dump(&stk);
+    stack_destructor(stk)
     return 0;
 }
 
